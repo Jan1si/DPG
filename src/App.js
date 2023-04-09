fd4baa8b7571f7946d5ffccedcadde0348802be4
Никита Евграфов <nikita.evgrafov@inbox.ru>
2023-04-09 14:15:03 +0500

    Сделана вёрстка

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="helloBlock">
+                    <h1>Hello</h1>
+                </div>
+                <div className="formBlock">
+                    <form>
+                        <section className="info">
+                            <p className="baseText">For business enquiries please use the form below</p>
+                            <p className="promtText">Required</p>
+                        </section>
+                        <section className="inputBlock">
+                            <div className="inputGroup">
+                                <label htmlFor="name">Name</label>
+                                <input type="text" name='name'/>
+                            </div>
+                            <div className="inputGroup">
+                                <label htmlFor="name">Company</label>
+                                <input type="text" name='company'/>
+                            </div>
+                            <div className="inputGroup">
+                                <label htmlFor="name">E-mail</label>
+                                <input type="email" name='email'/>
+                            </div>
+                            <div className="inputGroup">
+                                <label htmlFor="name">Phone</label>
+                                <input type="text" name='phone'/>
+                            </div>
+                            <div className="inputGroup">
+                                <label htmlFor="name">Message</label>
+                                <textarea name="message"></textarea>
+                            </div>
+                            <div className="checkBoxGroup">
+                                <div className="checkbox-wrapper">
+                                    <input id="customCheckBox" name='accept' type="checkbox"/>
+                                    <span></span>
+                                    <label htmlFor='customCheckBox'></label>
+                                </div>
+                                <label htmlFor="accept">I accept <p className='link'>Terms and Privacy Policy</p> </label>
+                            </div>
+                            <button className="btnSend">Send</button>
+                        </section>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
